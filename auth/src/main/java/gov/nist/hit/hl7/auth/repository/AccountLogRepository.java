04e42baf9d584d6ab76b769be679ab7c297b23f7
Sukchan Lee <acetcom@gmail.com>
2020-06-21 15:51:09 -0400

    update it

--- auth/src/main/java/gov/nist/hit/hl7/auth/repository/AccountLogRepository.java
+++ auth/src/main/java/gov/nist/hit/hl7/auth/repository/AccountLogRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import gov.nist.hit.hl7.auth.domain.Account;
+import gov.nist.hit.hl7.auth.domain.AccountLog;
 
-@RepositoryRestResource(collectionResourceRel = "account", path = "accountRest")
-public interface AccountRestRepository extends MongoRepository<Account, String> {
+@RepositoryRestResource(collectionResourceRel = "accountLog", path = "accountLog")
+public interface AccountLogRepository extends MongoRepository<AccountLog, String> {
 }
--- auth/src/main/java/gov/nist/hit/hl7/auth/repository/AccountRepository.java
+++ auth/src/main/java/gov/nist/hit/hl7/auth/repository/AccountRepository.java
@@ -1,12 +1,13 @@
 package gov.nist.hit.hl7.auth.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import gov.nist.hit.hl7.auth.domain.Account;
 
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "account", path = "account")
 public interface AccountRepository extends MongoRepository<Account, String> {
 
   public Account findByUsername(String username);
