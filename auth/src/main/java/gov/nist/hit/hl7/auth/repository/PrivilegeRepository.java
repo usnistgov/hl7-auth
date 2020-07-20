985db160aa524ce37598ab902d6a71960abc5da5
Sukchan Lee <acetcom@gmail.com>
2020-07-20 15:57:21 -0400

    Change ID to id

--- auth/src/main/java/gov/nist/hit/hl7/auth/repository/PrivilegeRepository.java
+++ auth/src/main/java/gov/nist/hit/hl7/auth/repository/PrivilegeRepository.java
@@ -1,11 +1,11 @@
 package gov.nist.hit.hl7.auth.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import gov.nist.hit.hl7.auth.domain.Privilege;
 
-@Repository
-
+@RepositoryRestResource(collectionResourceRel = "privilege", path = "privilege")
 public interface PrivilegeRepository extends MongoRepository<Privilege, String> {
 	
 	public Privilege findByRole(String role);
