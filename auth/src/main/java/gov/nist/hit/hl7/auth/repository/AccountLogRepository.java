aa70f6d97c8f7ff1499185927feee38da7b6c0e6
Sukchan Lee <acetcom@gmail.com>
2020-07-21 17:53:09 -0400

    Add findByDateBetweenAndFrom

--- auth/src/main/java/gov/nist/hit/hl7/auth/repository/AccountLogRepository.java
+++ auth/src/main/java/gov/nist/hit/hl7/auth/repository/AccountLogRepository.java
@@ -19,5 +19,6 @@ public interface AccountLogRepository extends MongoRepository<AccountLog, String
 
     List<AccountLog> findByDateBetween(@DateTimeFormat(pattern="yyyy-MM-dd") Date startDate, @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate);
     List<AccountLog> findByDateBetweenAndUsername(@DateTimeFormat(pattern="yyyy-MM-dd") Date startDate, @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate, String username);
+    List<AccountLog> findByDateBetweenAndFrom(@DateTimeFormat(pattern="yyyy-MM-dd") Date startDate, @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate, String from);
     List<AccountLog> findByDateBetweenAndUsernameAndFrom(@DateTimeFormat(pattern="yyyy-MM-dd") Date startDate, @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate, String username, String from);
 }
